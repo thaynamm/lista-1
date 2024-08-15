c2a45d4aa26918aa9ac2b6dfbefdd07a0049bd01
thaynamm <thaynamarcelinodossantos07@gmail.com>
2024-08-15 08:55:39 -0300

    Add files via upload

---
+++ exe1.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <table border = '3'>
+
+
+<?php
+$soma = 0;
+for ($i = 200; $i >= 0; $i--) {
+    if($i% 2 == 0){
+    
+    
+        echo "<tr><td>$i</td></tr>\n";
+    }
+    }
+    ?>
+</table>
+</body>
+</html>
